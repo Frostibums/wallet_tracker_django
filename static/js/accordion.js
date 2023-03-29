8596f9c616605a0e2171db396b829f942db409b3
Semenov Vladislav <frostibums@gmail.com>
2023-03-29 14:36:27 +0200

    Changed some logic in wallets_with_txs_list.html to make sure that chains logos display correctly. Changed accordion.js and tabs.js, so they work correctly.

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -1,14 +1,13 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
-
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+    if (panel.style.display === "flex") {
+      panel.style.display = "none";
     } else {
-      panel.style.maxHeight = "100%";
+      panel.style.display = "flex";
     }
   });
 }
